1b63e557f4c78bc397240b8e8db9ab6b1e05f9cd
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2025-03-12 20:18:26 +0100

    Add list handling and new classes A and B

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+List<string> list = ["test1"];
+string empty = String.Empty;
+
+foreach (string i in list){
+    Console.WriteLine(empty + i);
+    string s = GetMember(i);
+}
+
+var a = new A();
+
+string? GetMember(string parameter){
+    return null;
+}
